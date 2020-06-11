41a50e6243be53418c0963e3d1d6c9fda6b533ca
WJ75090983 <31917641+WJ75090983@users.noreply.github.com>
2020-06-11 15:09:48 +0800

    Add files via upload

---
+++ const.py
@@ -0,0 +1,10 @@
+#const.py
+const_image_width = 720
+const_image_height = 480
+
+const_use_batch_norm = True
+const_use_kernel_regularizer = False
+
+R_MEAN = 123.68
+G_MEAN = 116.78
+B_MEAN = 103.94
